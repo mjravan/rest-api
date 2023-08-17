166d1b114a0647aaf16a8d3a0a1e67eae125dbbd
Majed <majed79a@gmail.com>
2023-08-17 13:47:54 +0330

    added RF view and Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,19 @@
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from products.models import Product
+
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hi there, this is your Django API Response ;)"})
+    """
+    DRF API View
+    """
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data)
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,9 +2,9 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello world"})
-print(get_response.text)
-print(get_response.status_code)
+get_response = requests.get(endpoint, json={"product_id": 123 })
+# print(get_response.text)
+# print(get_response.status_code)
 
 
 # HTTP Request -> HTML
